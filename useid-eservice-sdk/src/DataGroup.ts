481fa8f0db3969d105708616b947d0e0a5da9477
Simon Kempendorf <s.kempendorf@tice-software.com>
2022-06-23 17:39:54 +0200

    Add simple readme to sdk

--- useid-eservice-sdk/src/DataGroup.ts
+++ useid-eservice-sdk/src/DataGroup.ts
@@ -1,3 +1,6 @@
+// DataGroups as defined by TR-03110 part 4, section 2.2.3
+// https://www.bsi.bund.de/SharedDocs/Downloads/EN/BSI/Publications/TechGuidelines/TR03110/BSI_TR-03110_Part-4_V2-2.pdf
+
 export enum DataGroup {
   DocumentType = "DG1",
   IssuingEntity = "DG2",
---
+++ useid-eservice-sdk/src/UseIdAPI.ts
@@ -0,0 +1,39 @@
+import { DataGroup } from "./DataGroup";
+import { IStartSessionResponse } from "./IStartSessionResponse";
+import { IdentityData, IIdentityDataValues } from "./IdentityData";
+
+export class UseIdAPI {
+  public static domain = "https://useid.dev.ds4g.net";
+  public static widgetSrc = `${UseIdAPI.domain}/widget.js`;
+
+  private static apiUrl = `${UseIdAPI.domain}/api/v1`;
+  private readonly apiKey: string;
+
+  constructor(apiKey: string) {
+    this.apiKey = apiKey;
+  }
+
+  // For now, the functions are mocking the behaviour
+  private mockDataGroups: DataGroup[] = [];
+  private sessionId?: string;
+
+  async startSession(refreshAddress: string, dataGroups: DataGroup[]): Promise<IStartSessionResponse> {
+    this.mockDataGroups = dataGroups;
+    this.sessionId = "96747088-55fa-411d-9993-092a0606ef89";
+    return {
+      tcTokenURL: UseIdAPI.apiUrl + `/provider/result/getTcToken.html;jsessionid=B6CDEDF68B4DAD7555187A1070770A8F`,
+      sessionId: this.sessionId
+    };
+  }
+
+  async getIdentityData(sessionId: string): Promise<IdentityData> {
+    if (sessionId !== this.sessionId) {
+      throw new Error("Identification Session Not Found");
+    }
+    const values: IIdentityDataValues = {};
+    this.mockDataGroups.forEach((value) => {
+      values[value] = `Placeholder for ${Object.keys(DataGroup)[Object.values(DataGroup).indexOf(value)]}`;
+    });
+    return new IdentityData(values);
+  }
+}
