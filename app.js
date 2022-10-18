b4fa003a495ea9482cb521bf9835e1af194b357e
Hannes Schaletzky <hannes.schaletzky@digitalservice.bund.de>
2022-10-18 15:38:05 +0200

    aligned code format

--- app.js
+++ app.js
@@ -6,16 +6,14 @@ app.set('view engine', 'pug');
 const { UseIdAPI, DataGroup } = require('useid-eservice-sdk');
 
 app.get('/health', async (req, res) => {
-  res.status(200).send("ok")
+  res.status(200).send('ok');
 });
 
 const useIdAPI = new UseIdAPI(process.env.USEID_API_KEY, process.env.USEID_DOMAIN);
 
 app.get('/', async (req, res) => {
   const useIdResponse = await useIdAPI.startSession();
-
   res.header('Content-Security-Policy', `frame-src ${useIdAPI.domain}; script-src ${useIdAPI.domain}`);
-
   return res.render('index', { widgetSrc: useIdAPI.widgetSrc, tcTokenUrl: useIdResponse.tcTokenUrl });
 });
 
@@ -24,20 +22,20 @@ app.get('/success', async (req, res) => {
   if (eIdSessionId === undefined) {
     return res.render('error', { errorMessage: 'No session found' });
   }
-  const resultMajor = req.query["ResultMajor"];
-  if (resultMajor === "error") {
-    return res.render("error", { errorMessage: "Identification failed. Please try again." });
+  const resultMajor = req.query.ResultMajor;
+  if (resultMajor === 'error') {
+    return res.render('error', { errorMessage: 'Identification failed. Please try again.' });
   }
   try {
     const identity = await useIdAPI.getIdentity(eIdSessionId);
-    let address = identity.get(DataGroup.PlaceOfResidence)["structuredPlace"];
+    const address = identity.get(DataGroup.PlaceOfResidence).structuredPlace;
     const data = [
       { key: 'Vorname', value: identity.get(DataGroup.GivenNames) },
       { key: 'Nachname', value: identity.get(DataGroup.FamilyNames) },
-      { key: 'Straße', value: address["street"] },
-      { key: 'PLZ', value: address["zipCode"] },
-      { key: 'Stadt', value: address["city"] },
-      { key: 'Land', value: address["country"] },
+      { key: 'Straße', value: address.street },
+      { key: 'PLZ', value: address.zipCode },
+      { key: 'Stadt', value: address.city },
+      { key: 'Land', value: address.country },
     ];
     return res.render('success', { data });
   } catch (e) {
