bf30abd62c94f2421973b163ab0a2c74ed702d94
mpanne <merlin.pannewitz@digitalservice.bund.de>
2023-01-25 17:31:12 +0100

    USEID-737: Add extra endpoint for QR code widget

--- app.js
+++ app.js
@@ -1,11 +1,18 @@
 require('dotenv').config();
 const express = require('express')
+const axios = require('axios');
 const app = express();
 app.set('views', require('path').join(__dirname, 'views'));
 app.set('view engine', 'pug');
 app.use(express.static('public'));
 
 const { UseIdAPI, DataGroup } = require('useid-eservice-sdk');
+
+axios.interceptors.request.use(config => {
+  config.headers = config.headers ?? {};
+  config.headers['Authorization'] = `Bearer ${process.env.USEID_API_KEY}`;
+  return config;
+});
 
 app.get('/health', async (req, res) => {
   res.status(200).send('ok');
@@ -44,5 +51,40 @@ app.get('/success', async (req, res) => {
     return res.render('error', { errorMessage: e.message });
   }
 });
+
+app.get('/qrcode', async (req, res) => {
+  const useIdResponse = await useIdAPI.startSession();
+  await createTransactionInfo(useIdResponse);
+  res.header('Content-Security-Policy', `frame-src ${useIdAPI.domain} mailto:; script-src ${useIdAPI.domain}`);
+  return res.render('index', { widgetSrc: `${useIdAPI.domain}/qrcode-widget.js`, tcTokenUrl: useIdResponse.tcTokenUrl });
+});
+
+async function createTransactionInfo(useIdResponse) {
+  let useIdSessionId = decodeURIComponent(useIdResponse.tcTokenUrl).match(
+      /sessions\/([a-f\d\-]*)\/tc-token/i
+  )[1];
+
+  console.log("Create transaction info for: " + useIdSessionId);
+
+  let data = {
+    "providerName": "Spaßkasse",
+    "providerURL": "https://www.sparkasse.de/",
+    "additionalInfo": [
+      {
+        "key": "Kundennummer",
+        "value": "23467812"
+      },
+      {
+        "key": "Nachname",
+        "value": "Musterfrau"
+      }
+    ]
+  };
+
+  let url = `${useIdAPI.domain}/api/v1/identification/sessions/${useIdSessionId}/transaction-info`;
+  const response = await axios.post(url, data);
+  console.log("Transaction info created successfully: " + JSON.stringify(response.data));
+}
+
 
 module.exports = app;
